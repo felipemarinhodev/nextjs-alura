3807660f5715be7637343862dff65165943c426a
felipemarinhodev <felipmarinho@gmail.com>
2022-05-12 14:07:53 -0700

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function HomePage() {
+	return (
+		<div>
+			<h1>Você caiu em uma página que não exite!!!! 😢</h1>
+			<Link href="/">Ir para Home</Link>
+		</div>
+	)
+}
